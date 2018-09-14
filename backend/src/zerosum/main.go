0fd6fb764c6b7c59ca596e80a66258fc91953ef3
tomforge <fooguowei3@gmail.com>
2018-09-15 00:57:32 +0800

    feat: Add api endpoint, middleware

--- backend/src/zerosum/main.go
+++ backend/src/zerosum/main.go
@@ -3,11 +3,34 @@ package main
 import (
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/graph-gophers/graphql-go"
+	"github.com/graph-gophers/graphql-go/relay"
+	"github.com/urfave/negroni"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"zerosum/repository"
 )
+
+func getSchema(path string) (string, error) {
+	b, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(b), nil
+}
 
 func main() {
 	_ = gin.Default()
 	err := repository.InitTestDB()
 	fmt.Print(err)
+	s, err := getSchema("schema/schema.graphql")
+	rootResolver := resolver.resolver // TODO: Add resolver path here
+	schema := graphql.MustParseSchema(s, &rootResolver)
+	apiHandler := relay.Handler{Schema: schema}
+	mux := http.NewServeMux()
+	mux.Handle("/query", &apiHandler)
+	n := negroni.Classic()
+	n.UseHandler(mux)
+	log.Fatal(http.ListenAndServe("localhost:8080", n))
 }
