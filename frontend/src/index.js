ceea4732acfc5bb312ddba8f42fed877616cdf20
tomforge <fooguowei3@gmail.com>
2018-09-23 01:27:27 +0800

    chore: Set up apollo for frontend graphql

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import ApolloClient from "apollo-boost"
+import {ApolloProvider} from "react-apollo"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const defaults = {
+};
+const client = new ApolloClient({
+  clientState: {
+    defaults: defaults
+  }
+}); //TODO: Set uri
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App/>
+  </ApolloProvider>,
+  document.getElementById('root'));
 registerServiceWorker();
