6171b20623c78fd0e5365351c7fbe3f35171ec35
tomforge <fooguowei3@gmail.com>
2018-09-24 15:28:52 +0800

    fix: Move privacy and terms to backend

--- backend/src/zerosum/main.go
+++ backend/src/zerosum/main.go
@@ -63,12 +63,15 @@ func main() {
 		os.Getenv("FACEBOOK_APP_ID"),
 		os.Getenv("FACEBOOK_ACCESS_TOKEN"),
 	)
+	staticFiles := packr.NewBox("./static")
+
 	authRouter := mux.NewRouter()
 	authRouter.Handle("/gql", gqlHandler)
 	an := negroni.New(auth.Auth.GetJwtMiddleware(), negroni.Wrap(authRouter))
 
 	router := mux.NewRouter()
 	router.HandleFunc("/login/facebook", auth.Auth.FbLoginHandler).Methods("POST")
+	router.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(staticFiles)))
 	if DEBUG {
 		router.Handle("/noauth/gql", gqlHandler)
 	}
