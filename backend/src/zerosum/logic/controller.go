ca08a24aa7b9d271e81e5cb3836e22065563c36b
tomforge <fooguowei3@gmail.com>
2018-09-26 12:16:57 +0800

    fix: Forgot to include schedule-next-game loop logic into game loop

--- backend/src/zerosum/logic/controller.go
+++ backend/src/zerosum/logic/controller.go
@@ -8,6 +8,7 @@ import (
 
 type GameController struct {
 	incomingGames  chan *models.Game
+	finishedGames  chan *models.Game
 	queue          TimedGameQueue
 	nextEndingGame *models.Game
 	timer          *time.Timer
@@ -18,36 +19,63 @@ var Controller *GameController
 func init() {
 	Controller = &GameController{
 		incomingGames: make(chan *models.Game, 100),
+		finishedGames: make(chan *models.Game, 100),
 		queue:         make(TimedGameQueue, 1),
 	}
-	go Controller.consumeIncoming()
+	go Controller.gameLoop()
 }
 
 func (c *GameController) AddGame(game *models.Game) {
 	c.incomingGames <- game
 }
 
-func (c *GameController) consumeIncoming() {
-	for game := range c.incomingGames {
-		if c.nextEndingGame == nil {
-			c.nextEndingGame = game
-			c.setTimer(game.EndTime)
-		} else if game.EndTime.Before(c.nextEndingGame.EndTime) {
-			if c.timer.Stop() {
-				// Only push if timer can be stopped, if false is returned nextEndingGame will already have been
-				// processed
-				heap.Push(&c.queue, c.nextEndingGame)
-			}
-			c.nextEndingGame = game
-			c.setTimer(game.EndTime)
-		} else {
-			heap.Push(&c.queue, game)
+func (c *GameController) consumeIncoming(game *models.Game) {
+	if c.nextEndingGame == nil {
+		c.nextEndingGame = game
+		c.setTimer(game)
+	} else if game.EndTime.Before(c.nextEndingGame.EndTime) {
+		// Add nextEndingGame back to the queue only if timer can be stopped (i.e. returns true) otherwise
+		// it means timer has (just) fired and nextEndingGame is not in the finishedGames chan
+		// Note: timer is never stopped unless there are no more games in the queue, at which
+		// point nextEndingGame will be nil, therefore if false is returned, only possibility is timer just fired, but
+		// the finished game is not yet processed (i.e. in the finishedGames chan)
+		if c.timer.Stop() {
+			heap.Push(&c.queue, c.nextEndingGame)
 		}
+		c.nextEndingGame = game
+		c.setTimer(game)
+	} else {
+		heap.Push(&c.queue, game)
 	}
 }
 
-func (c *GameController) setTimer(stopAt time.Time) {
-	c.timer = time.AfterFunc(stopAt.Sub(time.Now()), func() {
-		ResolveGame(c.nextEndingGame.Id)
-	})
+func (c *GameController) setTimer(game *models.Game) {
+	if !game.EndTime.After(time.Now()) {
+		c.finishedGames <- game
+	} else {
+		c.timer = time.AfterFunc(game.EndTime.Sub(time.Now()), func() {
+			c.finishedGames <- game
+		})
+	}
+}
+
+func (c *GameController) gameLoop() {
+	for {
+		select {
+		case game := <-c.incomingGames:
+			c.consumeIncoming(game)
+		case game := <-c.finishedGames:
+			// Schedule the next game, if it has not already been updated
+			if game == c.nextEndingGame {
+				if c.queue.Len() > 0 {
+					c.nextEndingGame = heap.Pop(&c.queue).(*models.Game)
+					c.setTimer(c.nextEndingGame)
+				} else {
+					// If there're no more games to process, nextEndingGame should be set to nil
+					c.nextEndingGame = nil
+				}
+			}
+			go ResolveGame(game.Id)
+		}
+	}
 }
