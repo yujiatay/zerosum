25e4723b6743360a9767b677db9fdb0673491d09
Yu Jia Tay <yujiatay@gmail.com>
2018-09-27 17:02:20 +0800

    feat: Add Sort dialog to Games Feed

--- frontend/src/components/Feed/GamesScreen.js
+++ frontend/src/components/Feed/GamesScreen.js
@@ -11,6 +11,14 @@ import Tab from "@material-ui/core/Tab/Tab";
 import GamesList from "./GamesList";
 import InfoBar from "./InfoBar";
 import ReactGA from 'react-ga';
+import Dialog from "@material-ui/core/Dialog/Dialog";
+import GameMode from "../CreateGame/GameMode";
+import DialogContent from "@material-ui/core/DialogContent/DialogContent";
+import StakesMode from "../CreateGame/StakesMode";
+import ButtonBase from "@material-ui/core/ButtonBase/ButtonBase";
+import Paper from "@material-ui/core/Paper/Paper";
+import CancelButton from "../CancelButton";
+import SubmitButton from "../SubmitButton";
 
 const styles = theme => ({
   root: {
@@ -35,14 +43,37 @@ const styles = theme => ({
   },
   tab: {
     width: '50%'
-  }
+  },
+  dialogTitle: {
+    color: '#014262',
+    marginBottom: 10
+  },
+  submit: {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: theme.spacing.unit * 2,
+    backgroundColor: '#014262'
+  },
+  buttonBase: {
+    flex: 1,
+    display: 'block'
+  },
+  innerSubmit: {
+    backgroundColor: 'transparent',
+    display: 'flex',
+    justifyContent: 'center',
+    padding: 5
+  },
 });
 
 class GamesScreen extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 0
+      value: 0,
+      filterDialog: false,
+      gmode: 'MAJORITY',
+      smode: 'FIXED_STAKES'
     }
   }
   componentDidMount() {
@@ -50,6 +81,26 @@ class GamesScreen extends Component {
   };
   handleChange = (event, value) => {
     this.setState({ value });
+  };
+  handleFilter = () => {
+    this.setState({
+      filterDialog: true
+    })
+  };
+  handleClose = () => {
+    this.setState({
+      filterDialog: false
+    });
+  };
+  handleGameMode = mode => {
+    this.setState({
+      gmode: mode
+    })
+  };
+  handleStakes = stake => {
+    this.setState({
+      smode: stake
+    })
   };
 
   render() {
@@ -89,6 +140,28 @@ class GamesScreen extends Component {
 
         {value === 0 && <GamesList/>}
         {value === 1 && <GamesList/>}
+
+        <Dialog
+          open={this.state.filterDialog}
+          onClose={this.handleClose}
+          aria-labelledby="form-dialog-title"
+        >
+          <CancelButton closeHandler={this.handleClose}/>
+          <DialogContent className={classes.queryDialog}>
+            <Typography variant="title" className={classes.dialogTitle} align="center">
+              Sort by
+            </Typography>
+            <Typography variant="title" align="left">
+              Game Mode
+            </Typography>
+            <GameMode modeHandler={this.handleGameMode}/>
+            <Typography variant="title" align="left">
+              Stakes Mode
+            </Typography>
+            <StakesMode clickHandler={this.handleStakes}/>
+            <SubmitButton submitHandler={this.handleSubmit}/>
+          </DialogContent>
+        </Dialog>
         <BottomNavBar value={0}/>
       </div>
     );
